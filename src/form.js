ef0a83dc8bc02b51deea6433f215bc10469e1cf0
tmtakashi <tmtakashi7@gmail.com>
2020-03-11 23:16:05 +0900

    added comment

--- src/form.js
+++ src/form.js
@@ -9,7 +9,7 @@ const Form = props => {
       case 'foo':
         const newFoo = event.target.checked
         setFoo(newFoo);
-        props.validate(newFoo && bar)
+        props.validate(newFoo && bar) // この時点でstateが更新されている保証がないので、別の変数に格納したものを使う
         break;
       case 'bar':
         const newBar = event.target.checked
